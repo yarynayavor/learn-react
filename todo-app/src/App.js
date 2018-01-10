9824be51b8aaa43078c1e863b674c24d720271ae
Yaryna Yavor <yourhurt09@gmail.com>
2018-01-10 21:23:57 +0200

    added todo app with delete and done statu , and also data saving

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
+import TaskList from './TaskList';
+import AddNewTask from './AddNewTask';
+import {Grid} from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        this.state= {
+            tasks:JSON.parse(localStorage.getItem('tasks')) || [],
+            addInputValue:''
+        }
+
+        this.onAddInputHandlerChange=this.onAddInputHandlerChange.bind(this);
+        this.addNewTask=this.addNewTask.bind(this);
+        this.deleteTask=this.deleteTask.bind(this);
+        this.onTaskClickHandler=this.onTaskClickHandler.bind(this);
+
+    }
+
+    onAddInputHandlerChange(e) {
+        this.setState({
+            addInputValue:e.target.value
+        })
+    }
+
+    addNewTask() {
+        let {tasks,addInputValue}=this.state;
+        if(addInputValue){
+            tasks.push({
+                title:addInputValue,
+                isDone:false,
+                id:uuid()
+            });
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        } else {
+            alert("write something");
+        }
+
+
+
+        this.setState({
+            tasks:JSON.parse(localStorage.getItem('tasks')),
+            addInputValue:''
+        })
+    }
+
+    deleteTask(_id) {
+        let {tasks}=this.state;
+        let _tasks=tasks.filter( task=> task.id!=_id);
+        localStorage.setItem('tasks', JSON.stringify(_tasks));
+
+        this.setState({
+            tasks:_tasks
+        })
+    }
+
+    //
+    // editTask() {
+    //
+    // }
+
+    onTaskClickHandler(_id) {
+        let {tasks}=this.state;
+        tasks.map((task) => {
+            if(_id===task.id) {
+                task.isDone=!task.isDone;
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                console.log(task);
+            }
+            return task;
+        });
+         this.setState({tasks});
+    }
+
+    render() {
+         const {addInputValue,tasks}=this.state;
+         return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+          <Grid>
+              <AddNewTask addInputValue={addInputValue}
+                          addNewTask={this.addNewTask}
+                          onAddInputHandlerChange={this.onAddInputHandlerChange}/>
+              <TaskList tasks={tasks} onTaskClickHandler={this.onTaskClickHandler} deleteTask={this.deleteTask}/>
+          </Grid>
       </div>
     );
   }
